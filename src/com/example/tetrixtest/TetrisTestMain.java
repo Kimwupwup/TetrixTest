84fe9070b42165d9fa3a9ba993c8fc3db3e3a487
Kimwupwup <wndud1640@naver.com>
2017-06-04 21:39:08 +0900

    ...

--- src/com/example/tetrixtest/TetrisTestMain.java
+++ src/com/example/tetrixtest/TetrisTestMain.java
@@ -3,16 +3,12 @@ package com.example.tetrixtest;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnTouchListener;
 import android.view.Window;
-import android.widget.Toast;
-import android.util.AttributeSet;
+
+
 
 public class TetrisTestMain extends Activity{
 
@@ -27,6 +23,7 @@ public class TetrisTestMain extends Activity{
 		/* game_intro를 가장먼서 view하고 화면을 클릭할 시에 게임 view가 출력된다.*/
 		setContentView(R.layout.game_intro);
 		introBtn = (ImageButton)findViewById(R.id.intro);		
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		
 		introBtn.setOnClickListener(new OnClickListener() {
 			@Override

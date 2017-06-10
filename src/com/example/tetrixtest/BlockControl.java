03b8fe9503f1b6798de7f490eac085be414a0fa1
Kimwupwup <wndud1640@naver.com>
2017-06-10 18:04:39 +0900

    Timer 고침(Handler, cancel수정, timerTaskMake함수 활용)

--- src/com/example/tetrixtest/BlockControl.java
+++ src/com/example/tetrixtest/BlockControl.java
@@ -63,40 +63,8 @@ public class BlockControl extends View{
 	private long dropTimer = 1000;
 	
 	private Timer timer;
+	private TimerTask timerTask;
 	
-	/*TimerTask따로 선언하였음*/
-	private TimerTask timerTask = new TimerTask() {
-
-		@Override
-		public void run() {
-			// TODO Auto-generated method stub
-			/*한칸 내린다*/
-			block.setCurrentY(block.getCurrentY() + 1);
-			
-			/*충돌이 일어날시에 이전의 상태로 복구한다.*/
-			if(isCollide() == true) {
-				block.setCurrentY(block.getCurrentY() - 1);
-				
-				/*다시 확인*/
-				block.setCurrentY(block.getCurrentY() + 1);
-				if(isCollide() == true) {
-					block.setCurrentY(block.getCurrentY() - 1);
-					
-					/*바닥이라는 것을 확인했으니 맵에 표시해주고 삭제될 줄이 있는지 확인한다.*/
-					setMap();
-					deleteLine();
-					
-					/*현재 나올 블럭의 이전의 nextBlock를 넣어주고, random으로 nextBlock을 만들어준다.*/
-					if (over == 0) {
-						block = new Block(4, 1, nextBlock.getBlockType());
-						nextBlock = new Block(16, 2, (int)(Math.random()*7) +1);
-					}
-				}
-			}
-			/*Handler를 이용해서 Timer마다 화면을 초기화하여 블럭을 움직이는 것처럼 보여준다.*/
-			mHandler.sendEmptyMessage(0);
-		}		
-	};
 
 	public BlockControl(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -106,7 +74,9 @@ public class BlockControl extends View{
 		nextBlock = new Block(16, 2, (int)(Math.random()*7) +1);
 		
 		/*타이머를 이용해서 일정 주기마다 한칸씩 내린다.*/
-		timer = new Timer(true);
+		timer = new Timer();
+		timerTask = timerTaskMake();
+		
 		timer.schedule(timerTask, 1000, dropTimer);
 	}
 	
@@ -129,12 +99,6 @@ public class BlockControl extends View{
 				 */
 				isOver();
 				
-				/*
-				 *맵을 갱신한 후, 타이머를 종료하고 게임을 멈춘다.
-				 *블럭이 새로생기는 것을 막고, 버튼이 작동하는 것을 막아야한다.
-				 *지금은 타이어만 멈추고 나머지는 전부 작동하는 중...
-				 */
-				isOver();
 				/* invalidate()
 				 * 이 함수는 불려질때마다 onDraw함수를 호출한다고 한다.
 				 * 바로 밑에 정의되어있는 onDraw함수를 호출하면서 그래픽이 움직이는 것처럼 보여지게 된다
@@ -144,6 +108,65 @@ public class BlockControl extends View{
 		}
 	};
 
+	/*
+	 * stage에 따른 속도를 조절하기 윈한 Handler이다.
+	 * 우선 사용하고 있던 timerTask를 멈추고 timerTaskMake함수를 이용하여 다시 작동시킬 Task를 넣어준다.
+	 * 이것은 timerTask를 멈추면 그 Task는 다시 작동시키는 것을 불가능하다고 한다.
+	 * 그리고 수정된 dropTimer를 넣어주고 다시 schedule를 시작한다.*/
+	Handler stageHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			if(msg.what == 0) {
+
+				timerTask.cancel();
+
+				timerTask = timerTaskMake();
+				timer.schedule(timerTask, 0, dropTimer);
+				
+				invalidate();
+			}
+		}
+	};
+	
+	/*
+	 * TimerTask를 만들어주는 함수
+	 * TimerTask가 한번 cancel되면 다음부터는 재사용이 불가능 하므로
+	 * 새로운 객체를 형성해줘야 한다고 한다.*/
+	public TimerTask timerTaskMake() {
+		
+		TimerTask temp = new TimerTask() {	
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				/*한칸 내린다*/
+				block.setCurrentY(block.getCurrentY() + 1);
+				
+				/*충돌이 일어날시에 이전의 상태로 복구한다.*/
+				if(isCollide() == true) {
+					block.setCurrentY(block.getCurrentY() - 1);
+					
+					/*다시 확인*/
+					block.setCurrentY(block.getCurrentY() + 1);
+					if(isCollide() == true) {
+						block.setCurrentY(block.getCurrentY() - 1);
+						
+						/*바닥이라는 것을 확인했으니 맵에 표시해주고 삭제될 줄이 있는지 확인한다.*/
+						setMap();
+						deleteLine();
+						
+						/*현재 나올 블럭의 이전의 nextBlock를 넣어주고, random으로 nextBlock을 만들어준다.*/
+						if (over == 0) {
+							block = new Block(4, 1, nextBlock.getBlockType());
+							nextBlock = new Block(16, 2, (int)(Math.random()*7) +1);
+						}
+					}
+				}
+				/*Handler를 이용해서 Timer마다 화면을 초기화하여 블럭을 움직이는 것처럼 보여준다.*/
+				mHandler.sendEmptyMessage(0);
+			}		
+		};
+		return temp;
+	}
+	
 	/* onDraw 
 	 * 실제 화면에 그래픽을 출력해준다.
 	 * 출력순서
@@ -431,10 +454,9 @@ public class BlockControl extends View{
 	public void calStage() {
 		if(nextStage < score) {
 			stage++;
-			dropTimer *= 0.1;	//확인용
+			dropTimer *= 0.7;	//확인용
 			nextStage += 100;
-			timer.cancel();
-			timer.schedule(timerTask, 1000, dropTimer);
+			stageHandler.sendEmptyMessage(0);
 		}
 	}
 	
@@ -451,12 +473,9 @@ public class BlockControl extends View{
 				 * 이렇게 하지 않을 경우, 게임이 끝나도 계속해서 블럭을 만들어낸다.
 				 * */
 				over = 1;
-				timer.cancel();
-				/* 스코어와 스테이지를 같이 출력해준다.*/
-
-				timer.cancel();
-				DialogDefault();
-
+				timerTask.cancel();
+				
+				/*게임이 끝날 경우 다시 시작할 것인지 나갈 것인지 확인하는 창을 띄운다.*/
 				DialogDefault();
 			}
 		}
@@ -498,10 +517,10 @@ public class BlockControl extends View{
 				stage = 1;
 				over = 0;
 				dropTimer = 1000;
-				mHandler.sendEmptyMessage(0);
+				stageHandler.sendEmptyMessage(0);
 				
-				timer = new Timer(true);
-				timer.schedule(timerTask, 1000, dropTimer);
+				timerTask = timerTaskMake();
+				stageHandler.sendEmptyMessage(0);
 			}
 		});
 		alter.show();
